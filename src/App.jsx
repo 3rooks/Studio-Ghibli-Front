e4d0be0bb9b993028f2c6f9b05aade1caf016152
3rooks <brookslynx@gmail.com>
2022-04-30 23:50:29 -0300

    Search Input (useState - Sass - Components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
+import MainContainer from './components/MainContainer';
+import { API_URL } from './constants/urlApi';
+
 const App = () => {
-	return <img src='/logo.svg' alt='' />;
+	const [data, setData] = useState();
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		fetch(API_URL)
+			.then((res) => res.json())
+			.then((res) => setData(res))
+			.catch((error) => console.log(error))
+			.finally(() => setLoading(true));
+	}, []);
+
+	if (!loading) return <Loading />;
+	return <MainContainer data={data} />;
 };
 
 export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+import style from './Loading.module.scss';
+
+const Loading = () => {
+	return <div className={style.loader}></div>;
+};
+
+export default Loading;
