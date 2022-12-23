a966a6e39b85f43ce943954e1d9ddf25da4a980a
3rooks <Brookslynx@gmail.com>
2022-12-22 23:35:50 -0300

    🐛fix fetching

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,16 +1,16 @@
 const URI = import.meta.env.VITE_BACK_URI;
 
 export const API_FETCH = {
-	USER_LOGIN: URI + '/api/login',
-	USER_REGISTER: URI + '/api/register',
-	GET_USER: URI + '/api/user',
-	GET_USER_PROFILE: URI + '/api/profile',
-	USER_CART: URI + '/api/carts',
-	GET_ALL_PRODUCTS: URI + '/api/products',
-	GET_PRODUCT: URI + '/api/products',
-	PATCH_IMG: URI + '/api/update-img',
-	PATCH_USERNAME: URI + '/api/update-username',
-	PATCH_EMAIL: URI + '/api/update-email',
-	PATCH_PASS: URI + '/api/update-password',
-	DELETE_USER: URI + '/api/unregister'
+	USER_LOGIN: URI + 'api/login',
+	USER_REGISTER: URI + 'api/register',
+	GET_USER: URI + 'api/user',
+	GET_USER_PROFILE: URI + 'api/profile',
+	USER_CART: URI + 'api/carts',
+	GET_ALL_PRODUCTS: URI + 'api/products',
+	GET_PRODUCT: URI + 'api/products',
+	PATCH_IMG: URI + 'api/update-img',
+	PATCH_USERNAME: URI + 'api/update-username',
+	PATCH_EMAIL: URI + 'api/update-email',
+	PATCH_PASS: URI + 'api/update-password',
+	DELETE_USER: URI + 'api/unregister'
 };
