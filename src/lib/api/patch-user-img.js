29a8edeab79a09b49f3853410cac634878f726d1
3rooks <Brookslynx@gmail.com>
2022-12-24 10:12:23 -0300

    ✅ last fix

--- src/lib/api/patch-user-img.js
+++ src/lib/api/patch-user-img.js
@@ -1,18 +1,16 @@
+import { API_FETCH } from '../../constants/urls';
 import emitEvent from '../events/alertEvent';
 import getUserProfile from './get-user-profile';
 
 const pathUserImg = async (token, user, setUser) => {
 	try {
-		const res = await fetch(
-			'https://desafios-backend-production.up.railway.app/api/update-image',
-			{
-				method: 'PATCH',
-				headers: {
-					authorization: `Bearer ${token}`
-				},
-				body: user
-			}
-		);
+		const res = await fetch(API_FETCH.PATCH_IMG, {
+			method: 'PATCH',
+			headers: {
+				authorization: `Bearer ${token}`
+			},
+			body: user
+		});
 
 		if (res.status === 202) {
 			await getUserProfile(token, setUser);
